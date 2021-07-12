29342a9a2ef3cea8a8c1f840fb7fc925b0f05798
Jeith <keithp1208@yahoo.com>
2021-07-11 21:51:33 -0500

    updating apiDomain

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const apiUrl = process.env.ENVIRONMENT === "production" ? "https://streamcents.com" : "http://localhost:1337";
+const apiUrl = process.env.ENVIRONMENT === "production" ? "https://pennystreams.com" : "http://localhost:1337";
 
 nFormatter = (num, digits) => {
     const lookup = [
